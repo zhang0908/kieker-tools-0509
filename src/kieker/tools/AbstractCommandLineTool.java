db41cda5f25c6529a10f36b81c6cc94758b1adab
unknown <564537499@qq.com>
2019-05-09 15:22:29 +0800

    origin

---
+++ src/kieker/tools/AbstractCommandLineTool.java
@@ -0,0 +1,138 @@
+/***************************************************************************
+ * Copyright 2017 Kieker Project (http://kieker-monitoring.net)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ***************************************************************************/
+
+package kieker.tools;
+
+import org.apache.commons.cli.BasicParser;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+
+import kieker.common.logging.Log;
+import kieker.common.logging.LogFactory;
+import kieker.tools.util.CLIHelpFormatter;
+import kieker.tools.util.ToolsUtil;
+
+/**
+ * @author Nils Christian Ehmke
+ *
+ * @since 1.10
+ */
+public abstract class AbstractCommandLineTool {
+
+	public static final String CMD_OPT_NAME_HELP_LONG = "help";
+	public static final String CMD_OPT_NAME_HELP_SHORT = "h";
+
+	public static final String CMD_OPT_NAME_VERBOSE_LONG = "verbose";
+	public static final String CMD_OPT_NAME_VERBOSE_SHORT = "v";
+
+	public static final String CMD_OPT_NAME_DEBUG_LONG = "debug";
+	public static final String CMD_OPT_NAME_DEBUG_SHORT = "d";
+
+	private static final Log LOG = LogFactory.getLog(AbstractCommandLineTool.class);
+
+	private final boolean useSystemExit;
+
+	public AbstractCommandLineTool(final boolean useSystemExit) {
+		this.useSystemExit = useSystemExit;
+	}
+
+	public final void start(final String[] args) {
+		final Options options = new Options();
+
+		this.addDefaultOptions(options);
+		this.addAdditionalOptions(options);
+
+		final CommandLine commandLine = this.parseCommandLineArguments(options, args);
+
+		boolean success;
+		if (null != commandLine) {
+			this.initializeLogger(commandLine);
+
+			if (commandLine.hasOption(CMD_OPT_NAME_HELP_SHORT)) {
+				this.printUsage(options);
+				System.exit(0);
+			}
+
+			// Using && instead of & should make sure that performTask is not executed when the readPropertiesFromCommandLine method returns false
+			success = this.readPropertiesFromCommandLine(commandLine);
+
+			if (!success) {
+				LOG.info("Use the option `--" + CMD_OPT_NAME_HELP_LONG + "` for usage information");
+			} else {
+				success = this.performTask();
+			}
+
+		} else {
+			success = false;
+		}
+
+		LOG.info("See 'kieker.log' for details");
+		if (!success && this.useSystemExit) {
+			System.exit(1);
+		}
+	}
+
+	private void initializeLogger(final CommandLine commandLine) {
+		if (commandLine.hasOption('d')) {
+			ToolsUtil.loadDebugLogger();
+		} else if (commandLine.hasOption('v')) {
+			ToolsUtil.loadVerboseLogger();
+		} else {
+			ToolsUtil.loadDefaultLogger();
+		}
+	}
+
+	private void printUsage(final Options options) {
+		final HelpFormatter formatter = this.getHelpFormatter();
+		formatter.printHelp(this.getClass().getName(), options, true);
+	}
+
+	private void addDefaultOptions(final Options options) {
+		options.addOption(new Option("v", "verbose", false, "verbosely prints additional information"));
+		options.addOption(new Option("d", "debug", false, "prints additional debug information"));
+		options.addOption(new Option(CMD_OPT_NAME_HELP_SHORT, CMD_OPT_NAME_HELP_LONG, false,
+				"prints the usage information for the tool, including available options"));
+	}
+
+	private CommandLine parseCommandLineArguments(final Options options, final String[] arguments) {
+		final BasicParser parser = new BasicParser();
+
+		try {
+			return parser.parse(options, arguments);
+		} catch (final ParseException ex) {
+			// Note that we append ex.getMessage() to the log message on purpose to improve the
+			// logging output on the console.
+			LOG.error("An error occurred while parsing the command line arguments: " + ex.getMessage(), ex);
+			LOG.info("Use the option `--" + CMD_OPT_NAME_HELP_LONG + "` for usage information");
+
+			return null;
+		}
+	}
+
+	protected abstract void addAdditionalOptions(Options options);
+
+	protected abstract boolean readPropertiesFromCommandLine(CommandLine commandLine);
+
+	protected abstract boolean performTask();
+
+	protected HelpFormatter getHelpFormatter() {
+		return new CLIHelpFormatter();
+	}
+
+}
---
+++ src/kieker/tools/util/RBridgeControl.java
@@ -0,0 +1,306 @@
+/***************************************************************************
+ * Copyright 2017 Kieker Project (http://kieker-monitoring.net)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ***************************************************************************/
+
+package kieker.tools.util;
+
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.io.UnsupportedEncodingException;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import org.math.R.Rsession;
+import org.rosuda.REngine.REXPDouble;
+import org.rosuda.REngine.REXPLogical;
+import org.rosuda.REngine.REXPMismatchException;
+import org.rosuda.REngine.REXPString;
+import org.rosuda.REngine.REXPVector;
+
+import kieker.common.logging.Log;
+import kieker.common.logging.LogFactory;
+
+/**
+ * @author Tillmann Carlos Bielefeld
+ *
+ * @since 1.10
+ */
+public final class RBridgeControl {
+
+	private static final Log LOG = LogFactory.getLog(RBridgeControl.class);
+	private static final AtomicInteger NEXTVARID = new AtomicInteger(1);
+
+	private Rsession rCon;
+
+	protected RBridgeControl() {
+		final OutputStream out = new OutputStream2StandardLog();
+		try {
+			this.rCon = Rsession.newLocalInstance(new PrintStream(out, true, "UTF-8"), null);
+		} catch (final UnsupportedEncodingException e) {
+			LOG.error(e.toString(), e);
+		}
+	}
+
+	/**
+	 * Wraps the execution of an arbitrary R expression. Both errors and results are logged.
+	 *
+	 * @param input
+	 *            The R expression to evaluate.
+	 *
+	 * @return The result or the error of the evaluation of the given R expression. The method tries to convert it into a string, if possible.
+	 */
+	public Object evalWithR(final String input) throws InvalidREvaluationResultException {
+		Object out = null;
+
+		try {
+			out = this.rCon.eval(input);
+
+			Object output = null;
+
+			if (out instanceof REXPString) {
+				output = ((REXPString) out).asString();
+			} else if (out instanceof REXPLogical) {
+				output = ((REXPLogical) out).toDebugString();
+			} else if (out != null) {
+				output = out;
+			} else {
+				throw new InvalidREvaluationResultException("Got a null result for evaluation input: \"" + input + "\"");
+			}
+
+			RBridgeControl.LOG.trace("> REXP: " + input + " return: " + output);
+		} catch (final REXPMismatchException exc) {
+			RBridgeControl.LOG.error("Error R expr.: " + input + " Cause: " + exc, exc);
+		}
+
+		return out;
+	}
+
+	/**
+	 *
+	 * @param variable
+	 *            variable to R
+	 * @throws InvalidREvaluationResultException
+	 */
+	public void toTS(final String variable) throws InvalidREvaluationResultException {
+		if (variable != null) {
+			final StringBuffer buf = new StringBuffer();
+			buf.append(variable);
+			buf.append(" <- ts(");
+			buf.append(variable);
+			buf.append(')');
+			this.evalWithR(buf.toString());
+		}
+	}
+
+	/**
+	 *
+	 * @param variable
+	 *            variable to R
+	 * @param frequency
+	 *            frequency to R
+	 * @throws InvalidREvaluationResultException
+	 */
+	public void toTS(final String variable, final long frequency) throws InvalidREvaluationResultException {
+		if (variable != null) {
+			final StringBuffer buf = new StringBuffer(21);
+			buf.append(variable);
+			buf.append(" <- ts(");
+			buf.append(variable);
+			buf.append(", frequency=");
+			buf.append(frequency);
+			buf.append(')');
+			this.evalWithR(buf.toString());
+		}
+	}
+
+	/**
+	 *
+	 * @param input
+	 *            string
+	 * @return {@link Double#NaN} in case of error, else dbvalue
+	 */
+	public double eDbl(final String input) {
+		final double resultOnFailure = Double.NaN;
+
+		try {
+			final Object evaluationResult = this.evalWithR(input);
+			final REXPDouble doubleResult = (REXPDouble) evaluationResult;
+			return doubleResult.asDouble();
+
+		} catch (final REXPMismatchException exc) {
+			RBridgeControl.LOG.error("Error casting value from R: " + input
+					+ " Cause: " + exc);
+			return resultOnFailure;
+		} catch (final InvalidREvaluationResultException exc) {
+			RBridgeControl.LOG.error(exc.getMessage(), exc);
+			return resultOnFailure;
+		}
+	}
+
+	/**
+	 *
+	 * @param input
+	 *            inputstring
+	 * @return Rdata
+	 */
+	public String eString(final String input) {
+		final String resultOnFailure = "";
+
+		try {
+			final Object evaluationResult = this.evalWithR(input);
+			final REXPString stringResult = (REXPString) evaluationResult;
+
+			return stringResult.toString();
+		} catch (final InvalidREvaluationResultException exc) {
+			RBridgeControl.LOG.error(exc.getMessage(), exc);
+			return resultOnFailure;
+		}
+	}
+
+	/**
+	 *
+	 * @param input
+	 *            inputstring
+	 * @return Rdata
+	 */
+	public double[] eDblArr(final String input) {
+		final double[] resultOnFailure = new double[0];
+
+		try {
+			final Object evaluationResult = this.evalWithR(input);
+			final REXPVector vectorResult = (REXPVector) evaluationResult;
+			return vectorResult.asDoubles();
+		} catch (final REXPMismatchException e) {
+			return resultOnFailure;
+		} catch (final InvalidREvaluationResultException e) {
+			RBridgeControl.LOG.error(e.getMessage(), e);
+			return resultOnFailure;
+		}
+	}
+
+	/**
+	 *
+	 * @param variable
+	 *            string
+	 *
+	 * @param values
+	 *            assign value
+	 * @throws InvalidREvaluationResultException
+	 */
+	public void assign(final String variable, final double[] values) throws InvalidREvaluationResultException {
+		final StringBuffer buf = new StringBuffer();
+		buf.append(variable);
+		buf.append(" <- c(");
+		boolean first = true;
+		for (final double item : values) {
+			if (!first) {
+				buf.append(',');
+			} else {
+				first = false;
+			}
+			buf.append(item);
+		}
+		buf.append(')');
+		this.evalWithR(buf.toString());
+	}
+
+	/**
+	 *
+	 * @param variable
+	 *            string
+	 * @param values
+	 *            assign vaules
+	 * @throws InvalidREvaluationResultException
+	 */
+	public void assign(final String variable, final Double[] values) throws InvalidREvaluationResultException {
+		final StringBuffer buf = new StringBuffer();
+		buf.append(variable);
+		buf.append(" <- c(");
+		boolean first = true;
+		for (final Double item : values) {
+			if (!first) {
+				buf.append(',');
+			} else {
+				first = false;
+			}
+			if ((null == item) || item.isNaN()) {
+				buf.append("NA");
+			} else {
+				buf.append(item);
+			}
+		}
+		buf.append(')');
+		this.evalWithR(buf.toString());
+	}
+
+	/**
+	 *
+	 * @param variable
+	 *            string
+	 * @param values
+	 *            assign vaules
+	 * @throws InvalidREvaluationResultException
+	 */
+	public void assign(final String variable, final Long[] values) throws InvalidREvaluationResultException {
+		final StringBuffer buf = new StringBuffer();
+		buf.append(variable);
+		buf.append(" <- c(");
+		boolean first = true;
+		for (final Long item : values) {
+			if (!first) {
+				buf.append(',');
+			} else {
+				first = false;
+			}
+			buf.append(item);
+		}
+		buf.append(',');
+		this.evalWithR(buf.toString());
+	}
+
+	/**
+	 * Returns a globally unique variable name, even during the access of multiple threads.
+	 *
+	 * @return A unique variable name of the form {@code var_1, var_2, ...}.
+	 */
+	public static String uniqueVarname() {
+		return String.format("var_%s", RBridgeControl.NEXTVARID.getAndIncrement());
+	}
+
+	/**
+	 * Delivers the singleton instance of this class.
+	 *
+	 * @return The singleton instance.
+	 */
+	public static final RBridgeControl getInstance() {
+		return LazyHolder.INSTANCE;
+	}
+
+	/**
+	 * This is a helper class, holding the singleton instance of {@link RBridgeControl} and making sure that the object is lazily created.
+	 *
+	 * @author Tillmann Carlos Bielefeld
+	 *
+	 * @since 1.10
+	 */
+	private static final class LazyHolder { // NOCS
+
+		static final RBridgeControl INSTANCE; // NOPMD (private modifier would require an additional getter method)
+
+		static {
+			INSTANCE = new RBridgeControl();
+		}
+
+	}
+}
